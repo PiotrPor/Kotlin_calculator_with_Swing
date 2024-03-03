c07f863bb76af9e660d5e9db5027bf622e25b311
PiotrPor <piotr.porembinski@outlook.com>
2024-03-03 08:55:33 +0100

    In process of adding buttons

--- src/main/kotlin/calculator/CalcWindow.kt
+++ src/main/kotlin/calculator/CalcWindow.kt
@@ -5,10 +5,19 @@ import javax.swing.JButton;
 import javax.swing.JComponent; //?
 
 class CalcWindow(window_title: String) : JFrame() {
+    //var klawisz_dodaj: JButton
+    var klawisze_dzialan : Array<JButton>
+    var klawisze_cyfr : Array<JButton>
+
     init {
         setTitle(window_title)
+        //
+        //klawisz_dodaj = JButton("+")
+        klawisze_dzialan = arrayOf(JButton("+"), JButton("-"),JButton("*"),JButton("/"))
+        klawisze_cyfr = arrayOfNulls<JButton>(10)
+        //
         defaultCloseOperation = EXIT_ON_CLOSE
-        setSize(600, 600)
-        setLocationRelativeTo(null)
+        setSize(800, 700)
+        setLocationRelativeTo(null) //window appears in the middle of the screen
     }
 }
